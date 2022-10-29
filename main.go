288aec1054823243fe7608cc0ace3428419930e9
robertmin1 <104002271+robertmin1@users.noreply.github.com>
2022-10-29 12:52:51 +0300

    Test Data Bash

--- main.go
+++ main.go
@@ -4,19 +4,19 @@ import "fmt"
 
 func main() {
 	rows := 10
-	var k int = 0	
-	for i := 1; i <= rows; i++ {		
-		k=0
+	var k int = 0
+	for i := 1; i <= rows; i++ {
+		k = 0
 		for space := 1; space <= rows-i; space++ {
-			fmt.Print("  ")			
+			fmt.Print("  ")
 		}
 		for {
 			fmt.Print("* ")
 			k++
-			if(k == 2*i-1){				
+			if k == 2*i-1 {
 				break
 			}
-		}		
+		}
 		fmt.Println("")
 	}
 }
